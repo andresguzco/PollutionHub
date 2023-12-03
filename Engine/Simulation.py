e3868f77128331b1869f2a10ef65c437b93c9efe
Andres <andres.guzman.cordero@hotmail.com>
2023-12-03 20:49:04 +0100

    Added simulation and simulated data

--- Engine/Simulation.py
+++ Engine/Simulation.py
@@ -1,8 +1,8 @@
-from Engine.Tools import phiFunction, relativeAngle, distanceCalc
-from typing import Dict, Tuple, List
 import matplotlib.pyplot as plt
 from itertools import product
 import matplotlib.cm as cm
+from typing import List
+from numba import jit, float64
 import numpy as np
 import random
 
@@ -13,74 +13,153 @@ class PollutionSimulation:
             N: int = 1000,
             Lag: int = 1,
             Phi: float = 0.35,
-            Rho: float = 0.5,
+            Rho: float = 0.50,
             timeInterval: int = 1,
             meanPol: float = 10.0,
             Distance: float = 10.0,
-            rowNormalise: bool = False
     ):
         random.seed(123)
         self.N: int = N
         self.Lag: int = Lag
         self.Phi: float = Phi
         self.Rho: float = Rho
-        self.Normalise = rowNormalise
         self.meanPol: float = meanPol
         self.Distance: float = Distance
         self.timeInterval: int = timeInterval
         self.GridSize: List[int] = [x for x in range(-1, 2)]
-        self.Location: Dict[int, Tuple[int, int]] = {
-            i: x for i, x in enumerate(list(product(self.GridSize, repeat=2)))
-        }
+        # The Location is now a 2D numpy ndarray instead of a dict
+        self.Location: np.ndarray = np.array(list(product(self.GridSize, repeat=2)))
         self.K: int = len(self.GridSize) ** 2
         self.windSpeed: np.ndarray = np.ones(shape=N) * 20
         self.windDirection: np.ndarray = np.random.uniform(low=0, high=360, size=[N])
         self.initialPollution: float = np.random.normal(loc=50, scale=2.5, size=[1, self.K])
         self.Y: np.ndarray = np.zeros([N, self.K])
 
-    def compute_phi_values(self) -> np.ndarray:
-        W = np.zeros((self.N, self.K, self.K))
-        for k in range(self.N):
-            for pair in product(list(self.Location.keys()), repeat=2):
-                i, j = pair
-                W[k, i, j] = 0 if i == j else phiFunction(
-                    phi=self.Phi,
-                    rho=self.Rho,
-                    distance=distanceCalc(
-                        coordinateA=self.Location[i],
-                        coordinateB=self.Location[j],
-                        unit=self.Distance
-                    ),
-                    n=self.timeInterval,
-                    velocity=self.windSpeed[k],
-                    angle=relativeAngle(
-                        coordinateA=self.Location[i],
-                        coordinateB=self.Location[j],
-                        windDir=self.windDirection[k]
-                    ),
-                    lag=self.Lag
-                )
-            if self.Normalise:
-                rowNorms: np.ndarray = np.linalg.norm(W[k, :, :], axis=1)
-                rowNorms[rowNorms == 0] = 1
-                W[k, :, :] = W[k, :, :] / rowNorms[:, np.newaxis]
-        return W
-
-    def simulate_pollution(self, W: np.ndarray) -> np.ndarray:
-        self.Y[0, :] = self.meanPol + self.initialPollution
-        for i in range(1, self.N):
-            self.Y[i, :] = (
-                    self.meanPol + W[i, :, :] @ self.Y[i - 1, :].transpose() +
-                    np.random.normal(loc=0, scale=1, size=[1, self.K]))
-        return self.Y
+    def computePhi(self) -> np.ndarray:
+        return self._phiComputation(
+            Phi=self.Phi,
+            Rho=self.Rho,
+            timeInterval=self.timeInterval,
+            windSpeed=self.windSpeed,
+            windDirection=self.windDirection,
+            Lag=self.Lag,
+            N=self.N,
+            K=self.K,
+            # We pass the numpy array Location to the Numba function
+            Location=self.Location,
+            Distance=self.Distance
+        )
 
     @staticmethod
-    def plot_results(Y: np.ndarray) -> None:
+    # Update the method signature to receive a np.ndarray instead of a Dict
+    @jit(nopython=True)
+    def _phiComputation(
+            Phi: float,
+            Rho: float,
+            timeInterval,
+            windSpeed: np.ndarray,
+            windDirection: np.ndarray,
+            Lag: int,
+            N: int,
+            K: int,
+            Location: np.ndarray,
+            Distance: float
+    ) -> np.ndarray:
+        W: np.ndarray = np.zeros((N, K, K))
+        for k in range(N):
+            for i in range(K):  # Iterate over i and j in range(K)
+                for j in range(K):
+                    W[k, i, j] = 0 if i == j else phiFunction(
+                        phi=Phi,
+                        rho=Rho,
+                        distance=distanceCalc(
+                            coordinateA=Location[i],
+                            coordinateB=Location[j],
+                            unit=Distance
+                        ),
+                        n=timeInterval,
+                        velocity=windSpeed[k],
+                        angle=relativeAngle(
+                            coordinateA=Location[i],
+                            coordinateB=Location[j],
+                            windDir=windDirection[k]
+                        ),
+                        lag=Lag
+                    )
+        return W
+
+    def simulateVar(self, W_input: np.ndarray) -> np.ndarray:
+        Yinput: np.ndarray = np.zeros([self.N, self.K])
+        return self._simComputation(
+            N=self.N,
+            K=self.K,
+            meanPol=self.meanPol,
+            initialPollution=self.initialPollution,
+            W=W_input,
+            Y=Yinput
+        )
+
+    @staticmethod
+    @jit(nopython=True)
+    def _simComputation(
+            N: int,
+            K: int,
+            meanPol: float,
+            initialPollution: float,
+            W: np.ndarray,
+            Y: np.ndarray
+    ) -> np.ndarray:
+        Y[0, :] = meanPol + initialPollution
+        for i in range(1, N):
+            Y[i, :] = (
+                    meanPol + np.dot(W[i, :, :], Y[i - 1, :].transpose()) +
+                    np.random.normal(loc=0, scale=1, size=(1, K)))
+        return Y
+
+    @staticmethod
+    def plot_results(Y: np.ndarray, filepath: str) -> None:
+        if not np.all(np.isfinite(Y)) or np.all(Y == Y[0]):
+            print(f"Invalid data in Y, skipping plot for {filepath}")
+            return None
+
         fig, ax = plt.subplots()
         cmap = cm.get_cmap('viridis')
         colors = [cmap(i) for i in np.linspace(0, 1, Y.shape[1])]
         for i in range(Y.shape[1]):
             ax.plot(Y[:, i], color=colors[i])
-        plt.show()
+        plt.savefig(filepath)
+        plt.clf()
+        return None
 
 
+@jit(nopython=True)
+def relativeAngle(coordinateA, coordinateB, windDir):
+    coordinateA = coordinateA.astype(float64)
+    coordinateB = coordinateB.astype(float64)
+    v1_u = coordinateA / np.linalg.norm(coordinateA)
+    v2_u = coordinateB / np.linalg.norm(coordinateB)
+    dot_product = np.dot(v1_u, v2_u)
+    clipped_dot_product = max(min(dot_product, 1.0), -1.0)
+    return np.arccos(clipped_dot_product) - windDir
+
+
+@jit(nopython=True)
+def phiFunction(
+        phi: float,
+        rho: float,
+        distance: float,
+        n: int,
+        velocity: np.ndarray,
+        angle: float,
+        lag: int
+) -> float:
+    result = phi - rho * (((distance * n) / (velocity * np.cos(angle))) - lag) ** 2
+    return result if result > 0 else 0.0
+
+
+@jit(nopython=True)
+def distanceCalc(coordinateA, coordinateB, unit):
+    coordinateA = coordinateA.astype(float64)
+    coordinateB = coordinateB.astype(float64)
+    diff = coordinateA - coordinateB
+    return np.sqrt(np.dot(diff, diff)) * unit
--- Interface/Simulation_Interface.py
+++ Interface/Simulation_Interface.py
@@ -1,11 +1,49 @@
 from Engine.Simulation import PollutionSimulation
+from numpy import ndarray, linspace
+from tqdm import tqdm
+
+
+def gridSearch(
+        phi_min: float,
+        phi_max: float,
+        rho_min: float,
+        rho_max: float,
+        num_steps: int
+):
+    phi_values: ndarray = linspace(phi_min, phi_max, num_steps)
+    rho_values: ndarray = linspace(rho_min, rho_max, num_steps)
+
+    for phi in phi_values:
+        for rho in tqdm(rho_values):
+            try:
+                simulation: PollutionSimulation = PollutionSimulation(
+                    N=1000,
+                    Lag=1,
+                    Phi=phi,
+                    Rho=rho,
+                    timeInterval=1,
+                    meanPol=10.0,
+                    Distance=10.0
+                )
+                W: ndarray = simulation.computePhi()
+                Y: ndarray = simulation.simulateVar(W)
+
+                filename: str = f"../Output/phi_{phi:.2f}_rho_{rho:.2f}.png"
+                simulation.plot_results(Y, filename)
+            except OverflowError:
+                print(f"Overflow error for Phi = {phi:.2f}, Rho = {rho:.2f}. Skipping.")
+                continue
+    return None
 
 
 def main() -> None:
-    simulation = PollutionSimulation()
-    W = simulation.compute_phi_values()
-    Y = simulation.simulate_pollution(W)
-    simulation.plot_results(Y)
+    gridSearch(
+        phi_min=0.00,
+        phi_max=1.00,
+        rho_min=0.00,
+        rho_max=1.00,
+        num_steps=10
+    )
     return None
 
 
