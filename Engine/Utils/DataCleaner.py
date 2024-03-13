9358b269dec5d737ac33a65fa2d3bd3cd6465764
Andres <andres.guzman.cordero@hotmail.com>
2024-03-13 14:55:33 +0100

    Pushing the finalised cleaning of the data

--- Engine/Utils/DataCleaner.py
+++ Engine/Utils/DataCleaner.py
@@ -1,86 +1,102 @@
 from __future__ import annotations
 
 from Engine.Utils import SpatialTools
+from itertools import product
 import pandas as pd
+import numpy as np
 
 
 class RunFlow:
     def __init__(self) -> None:
         self.geo_lev = "tag"
         self.time_lev = "timestamp"
+        self.cutoff: str = '2019-09-19 01:00:00+00:00'
 
-        self.angle_matrix = None
-        self.weight_matrix = None
-        self.coordinate_dict = None
-
-        self.raw_data = None
-        self.grouped_data = None
+        self.data = None
         self.processed_data = None
-        self.processed_data = {
-            "Train Data": None,
-            "Test Data": None,
-            "Validation Data": None
-        }
 
     def get_data(self, path: str, faulty: set | list) -> None:
-        self.raw_data = pd.read_csv(path)
+        self.data = pd.read_csv(path)
         self.format_data(faulty=faulty)
         self.group_data()
-        # self.coordinate_dict = SpatialTools.coordinate_dict(df=self.grouped_data,  geo_level=self.geo_lev)
-        # self.weight_matrix, self.angle_matrix = SpatialTools.weight_angle_matrix(self.coordinate_dict)
+        distance_matrix, angle_matrix, loc_dict = SpatialTools.weight_angle_matrix(
+            df=self.data,
+            geo_level=self.geo_lev
+        )
+        names = [element[0] + "_" + element[1] for element in list(product(list(loc_dict.keys()), repeat=2))]
+
+        windAngle = self.process_data()
+
+        angles = SpatialTools.spatial_tensor(
+            angle=windAngle, angle_matrix=angle_matrix
+        )
+        angles.columns = ["Angle | " + element for element in names]
+        angles.index = self.data.index
+
+        distance_matrix = pd.DataFrame(np.tile(distance_matrix.flatten(), (len(windAngle), 1)))
+        distance_matrix.index = self.data.index
+        distance_matrix.columns = ["Distance | " + element for element in names]
+
+        self.data = pd.concat([self.data, distance_matrix, angles], axis=1)
         return None
 
+    def process_data(self) -> pd.DataFrame:
+        def formatLoc(df):
+            location = df['tag'].unique()[0]
+            cols = [element + '_' + location for element in df.columns.tolist()]
+            df.columns = cols
+            return df.drop(columns=['tag_' + location], axis=1)
+
+        def mergeLocations(df):
+            locations = {loc: formatLoc(df[df['tag'] == loc]) for loc in list(df['tag'].unique())}
+            return pd.concat([locations[loc] for loc in locations.keys()], axis=1)
+
+        canvasDf = self.data.copy()
+        canvasDf.drop(["latitude", "longitude"], axis=1, inplace=True)
+
+        windSpeed = canvasDf.loc[:, "Wind Speed"]
+        windInfo = windSpeed.reset_index().groupby('timestamp').mean()
+        windAngle = canvasDf.loc[self.cutoff:, "Wind Angle"]
+        windAngle = windAngle.reset_index().groupby('timestamp').mean()
+        canvasDf.drop(['Wind Angle', 'Wind Speed'], axis=1, inplace=True)
+
+        self.data: pd.DataFrame = pd.concat(
+            [mergeLocations(canvasDf).loc[self.cutoff:], windInfo.loc[self.cutoff:]],
+            axis=1)
+
+        return windAngle
+
     def format_data(self, faulty: set | list) -> None:
-        self.raw_data["FH"] *= 0.36
-        self.raw_data.drop([
+        self.data["FH"] *= 0.36
+        self.data.drop([
             'id', 'no2', 'pm10', 'pm10_cal', 'pm10_fac', 'pm10_max', 'pm10_min',
             'datum', 'tijd', 'components', 'sensortype', 'weekdag', 'uur', '#STN',
             'jaar', 'maand', 'weeknummer', 'dag', 'H', 'T', 'U', 'YYYYMMDD', 'name'
         ], axis=1, inplace=True)
 
-        self.raw_data.rename(columns={"DD": "Wind Angle", "FH": "Wind Speed"}, inplace=True)
+        self.data.rename(columns={"DD": "Wind Angle", "FH": "Wind Speed"}, inplace=True)
         self.delete_entries_raw(pop_values=faulty, key=self.geo_lev)
-        self.raw_data[self.time_lev] = pd.to_datetime(self.raw_data[self.time_lev])
+        self.data[self.time_lev] = pd.to_datetime(self.data[self.time_lev])
         return None
 
     def delete_entries_raw(self, pop_values: set | list, key: str) -> None:
         if len(pop_values) > 0:
-            self.raw_data = self.raw_data[~self.raw_data[key].isin(pop_values)]
+            self.data = self.data[~self.data[key].isin(pop_values)]
         else:
             pass
 
     def group_data(self) -> None:
-        grouped_df = self.raw_data.groupby(by=[self.geo_lev, self.time_lev]).mean().reset_index()
+        grouped_df = self.data.groupby(by=[self.geo_lev, self.time_lev]).mean().reset_index()
         grouped_df.set_index(self.time_lev, inplace=True, drop=True)
-        self.grouped_data = grouped_df
+        self.data = grouped_df
         return None
 
-    def run(self) -> None:
-        path = r"../DTO/pm25_weer.csv"
-
+    def run(self, path_to_save: str, path_to_read: str) -> None:
         no_sensors = ["Uithoorn", "Velsen-Zuid", "Koog aan de Zaan", "Wijk aan Zee"]
 
-        self.get_data(path=path, faulty=no_sensors)
-        # self.split()
-        return None
-
-    def split(self, separation: float = 0.75) -> None:
-        self.processed_data["Train Data"]: pd.DataFrame = ...
-        self.processed_data["Test Data"]: pd.DataFrame = ...
-        self.processed_data["Validation Data"]: pd.DataFrame = ...
-        self.delete_entries()
-        return None
-
-    def delete_entries(self) -> None:
-        train_names = set(self.processed_data["Train Data"][self.geo_lev].unique())
-        test_names = set(self.processed_data["Test Data"][self.geo_lev].unique())
-        val_names = set(self.processed_data["Validation Data"][self.geo_lev].unique())
-
-        misplaced_train = train_names - (test_names & train_names & val_names)
-        misplaced_test = test_names - (test_names & train_names & val_names)
-        misplaced_test = test_names - (test_names & train_names & val_names)
-
-        # TODO: Drop all the rows that contain unique locations
+        self.get_data(path=path_to_read, faulty=no_sensors)
+        self.data.ffill(inplace=True)
+        self.data.to_csv(path_to_save)
         return None
 
 
--- Engine/Utils/SpatialTools.py
+++ Engine/Utils/SpatialTools.py
@@ -7,25 +7,22 @@ from math import sin, atan2
 from numpy import cos
 
 
-def get_bearing(coor1: list, coor2: list) -> float:
-    d_lon = (coor2[1] - coor1[1])
-    y = sin(d_lon) * cos(coor2[0])
-    x = cos(coor1[0]) * sin(coor2[0]) - sin(coor1[0]) * cos(coor2[0]) * cos(d_lon)
-    brng = atan2(y, x)
-    brng = rad2deg(brng)
-    return brng
+def get_bearing(coordinate_1: list, coordinate_2: list) -> float:
+    d_lon = (coordinate_2[1] - coordinate_1[1])
+    y = sin(d_lon) * cos(coordinate_2[0])
+    x = cos(coordinate_1[0]) * sin(coordinate_2[0]) - sin(coordinate_1[0]) * cos(coordinate_2[0]) * cos(d_lon)
+    bearing = atan2(y, x)
+    bearing = rad2deg(bearing)
+    return bearing
 
 
-def coordinate_dict(df: DataFrame, geo_level: str):
+def weight_angle_matrix(df: DataFrame, geo_level: str) -> tuple[ndarray, ndarray, dict]:
     locations = list(df[geo_level].unique())
-    c_dict = dict()
+    loc_dict = dict()
     for item in locations:
-        c_dict[item] = [df.loc[df[geo_level] == item, 'latitude'].mean(),
-                        df.loc[df[geo_level] == item, 'longitude'].mean()]
-    return c_dict
+        loc_dict[item] = [df.loc[df[geo_level] == item, 'latitude'].mean(),
+                          df.loc[df[geo_level] == item, 'longitude'].mean()]
 
-
-def weight_angle_matrix(loc_dict: dict) -> tuple[ndarray, ndarray]:
     n = len(loc_dict)
     w_matrix = zeros((n, n))
     angle_matrix = zeros((n, n))
@@ -33,10 +30,24 @@ def weight_angle_matrix(loc_dict: dict) -> tuple[ndarray, ndarray]:
     for i, value1 in enumerate(loc_dict):
         for j, value2 in enumerate(loc_dict):
             if i != j:
-                theta = get_bearing(loc_dict[value1], loc_dict[value2])
+                angle_matrix[i, j] = get_bearing(loc_dict[value1], loc_dict[value2])
                 w_matrix[i, j] = geodesic(loc_dict[value1], loc_dict[value2]).km
-                angle_matrix[i, j] = theta
             else:
                 w_matrix[i, i] = 0
                 angle_matrix[i, i] = 0
-    return w_matrix, angle_matrix
+    return w_matrix, angle_matrix, loc_dict
+
+
+def spatial_tensor(
+        angle: DataFrame,
+        angle_matrix: ndarray
+        ) -> DataFrame:
+
+    angle = angle.to_numpy()
+    t = len(angle)
+    n = len(angle_matrix)
+    ww_tensor = zeros((t, n, n))
+
+    for i in range(t):
+        ww_tensor[i, :, :] = cos(angle_matrix - float(angle[i]))
+    return DataFrame(ww_tensor.reshape(t, n * n))
