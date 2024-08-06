c2ce9155266cf5aeff116ee23bd45d07110b8e8d
Andres <andres.guzman.cordero@hotmail.com>
2024-08-05 23:34:51 -0400

    Implemented new idea, reconnecting to database later this week to test it

--- Engine/STFNet.py
+++ Engine/STFNet.py
@@ -16,64 +16,62 @@ class STFNet(nn.Module):
             nn.ReLU(),
             nn.Linear(512, 512),
             nn.ReLU(),
-            nn.Linear(512, (self.output_dim ** 2) + 2)
+            nn.Linear(512, self.output_dim ** 2),
+            nn.ReLU()
         )
+        self.vars = torch.Parameter(torch.randn(2))
 
     def forward(self, Y, Z):
         output = self.network(Z)
-        T_flat = output[:, :-2].detach()
-        params = output[:, -2:].exp().detach()
+        T_flat = output.detach()
+        params = self.vars
 
-        T = T_flat.reshape(output.shape[0], self.output_dim, self.output_dim)
-        H = torch.zeros(output.shape[0], self.output_dim, self.output_dim)
-        Q = torch.zeros(output.shape[0], self.output_dim, self.output_dim)
+        T = T_flat.reshape(output.shape[0], self.output_dim, self.output_dim) 
+        H = torch.eye(self.output_dim).unsqueeze(0).expand(output.shape[0], -1, -1) * params[0]
+        Q = torch.eye(self.output_dim).unsqueeze(0).expand(output.shape[0], -1, -1) * params[1]
 
-        for i in range(output.shape[1]):
-            H[i, :, :] = torch.eye(self.output_dim) * params[i, 0]
-            Q[i, :, :] = torch.eye(self.output_dim) * params[i, 1]
+        y_out = self._KalmanFilter(Y, T, H, Q)
+        return y_out
 
-        return self.KalmanFilter(Y, T, H, Q)
-
-    @staticmethod
-    @torch.no_grad()
-    def predict(alpha_prev, P_prev, Q, T_t):
+    def predict(self, alpha_prev, P_prev, Q, T_t):
         alpha_pred = torch.matmul(T_t, alpha_prev)
         first_part = torch.matmul(T_t, P_prev)
         P_new = torch.matmul(first_part, T_t.t()) + Q
         return alpha_pred, P_new
 
-    @staticmethod
-    @torch.no_grad()
-    def update(alpha_pred, P_pred, y, H):
+
+    def update(self, alpha_pred, P_pred, y, H):
         K = torch.matmul(P_pred, torch.inverse(P_pred + H))
         alpha_upd = alpha_pred + torch.matmul(K, (y - alpha_pred).t()).t()
         P_upd = torch.eye(K.size(0)) - K.matmul(P_pred)
         return alpha_upd, P_upd
 
-    @torch.no_grad()
-    def KalmanFilter(self, Y, T, Q, H):
+    def _KalmanFilter(self, Y, T, Q, H):
         n_timesteps = len(Y)
         n_states = self.output_dim
 
-        alpha_estimates = torch.zeros(n_timesteps, n_states)
-        P_estimates = torch.zeros(n_timesteps, n_states, n_states)
-
-        alpha_estimates[0, :] = torch.ones(self.output_dim)
-        P_estimates[0, :, :] = torch.eye(self.output_dim)
+        alpha_estimates = [torch.ones(1, self.output_dim)]
+        P_estimates = [torch.eye(self.output_dim).unsqueeze(0)]
 
         for t in range(1, n_timesteps):
             alpha_t, P_t = self.predict(
-                alpha_prev=alpha_estimates[t-1, :],
-                P_prev=P_estimates[t-1, :, :],
+                alpha_prev=alpha_estimates[-1].squeeze(0),
+                P_prev=P_estimates[-1].squeeze(0),
                 Q=Q[t, :, :],
                 T_t=T[t-1, :, :]
             )
 
-            alpha_estimates[t, :], P_estimates[t, :, :] = self.update(
+            alpha_updated, P_updated = self.update(
                 alpha_pred=alpha_t,
                 P_pred=P_t,
                 y=Y[t, :],
                 H=H[t, :, :]
             )
+
+            alpha_estimates.append(alpha_updated.unsqueeze(0))
+            P_estimates.append(P_updated.unsqueeze(0))
+
+        alpha_estimates = torch.cat(alpha_estimates, dim=0)
+        P_estimates = torch.cat(P_estimates, dim=0)
 
         return alpha_estimates, P_estimates
